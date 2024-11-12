5038a624ff08625a29a138fc29798815faded534
Nehad <nehad.st@gmail.com>
2024-11-11 22:38:10 -0500

    Created a centralized database configuration to avoid circular dependcies between app.js and routes and to keep the database conenction logic in one place. Some key components include database connecting config and creating connecting pool and exported promisified pool

---
+++ routes/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const dbConfig = {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+};
+
+const pool = mysql.createPool(dbConfig);
+
+module.exports = {
+    pool: pool.promise()
+};
