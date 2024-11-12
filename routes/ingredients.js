834acb567131274377bf570c4b36f643751e587b
Nehad <nehad.st@gmail.com>
2024-11-11 22:39:49 -0500

    Modified ingredients.js to import the pool from db.js instead of app.js

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { pool } = require('../app');
+const { pool } = require('../db');
 
 // Get all ingredients
 router.get('/', async (req, res) => {
