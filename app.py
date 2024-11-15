4ba8ce132cc71098ea4aed5a90dea0d255b60b7d
Nehad <nehad.st@gmail.com>
2024-11-14 22:42:15 -0500

    Added class Ingredients to app.py which represents the Ingredients table in the database

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///burgertone_inventory.db'
 db = SQLAlchemy(app)
 
+class Ingredients(db.Model):
+    id = db.Column(d.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    unit = db.Column(db.String(255), nullable=False)
+    quantity = db.Column(db.Float(10,2), nullable=False)
+    threshold = db.Column(db.Float(10,2), nullable=False)
+
 @app.route('/')
 def home():
     return "Hello, Flask!"
