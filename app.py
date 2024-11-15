77917ac4d760aab31d8eddb1f1aa2d868e9a27cc
Nehad <nehad.st@gmail.com>
2024-11-14 22:26:10 -0500

    Added more routes to app.py like the api data route and also install Flask-SQLAlchemy, #5

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+@app.route('/')
 def home():
     return "Hello, Flask!"
 
+@app.route('/api/data')
+def get_data():
+    return {"data": "This is your data"}
+
+@app.route('/api/hello/<name>')
+def say_hello(name):
+    return {"message": f"Hello, {name}!"}
+
+
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(debug=True)
