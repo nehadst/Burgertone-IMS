82220db915ad6e716e309e44fb8920ed00e5d934
Nehad <nehad.st@gmail.com>
2024-11-15 20:10:20 -0500

    Added adding new ingredients, updating an ingredient by id, and updating it ideally the quantity, however, you can pretty much update everything about it.

--- app/routes/ingredients.py
+++ app/routes/ingredients.py
@@ -20,3 +20,46 @@ def get_ingredient(id):
     if not ingredient:
         return jsonify({'message': 'Ingredient not found'}), 404
     return jsonify(ingredient.__dict__)
+
+# Adding a new ingredient
+@ingredients_blueprint.route('/', methods = ['POST'])
+def add_ingredient():
+    data = request.get_jsoN()
+    new_ingredient = Ingredients(
+        name = data['name'],
+        unit = data['unit'],
+        quantity = data['quantity'],
+        threshold = data['threshold'])
+    db.session.add(new_ingredient)
+    db.session.commit()
+
+    return jsonify(new_ingredient.__dict__), 201
+
+
+# Update an ingredient by ingredient id
+@ingredients_blueprint.route('/<int:id>', methods = ['PUT'])
+def add_ingredient():
+    ingredient = Ingredients.query.get(id)
+    if not ingredient:
+        return jsonify({'message': 'Ingredient not found'}), 404
+    data = request.get_jsoN()
+    ingredient.name = data['name']
+    ingredient.unit = data['unit']
+    ingredient.quantity = data['quantity']
+    ingredient.threshold = data['threshold']
+    db.session.commit()
+
+    return jsonify(ingredient.__dict__)
+
+
+
+# Delete an ingredient by ingredient id
+@ingredients_blueprint.route('/<int:id>', methods = ['DELETE'])
+def delete_ingredient(id):
+    ingredient = Ingredients.query.get(id)
+    if not ingredient:
+        return jsonify({'message': 'Ingredient not found'}), 404
+    
+    db.session.delete(ingredient)
+    db.session.commit()
+    return jsonify({'message': 'Ingredient deleted'})
